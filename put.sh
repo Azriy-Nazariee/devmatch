1b57d196c1d42a1529d14d012852658c702fc4f9
Azriy-Nazariee <azriynazariee@gmail.com>
2026-01-23 17:11:05 +0800

    feat: initialize NestJS application with profiles management
    - Add package.json for project dependencies and scripts
    - Create post.sh and put.sh for testing profile creation and updates
    - Implement AppController and AppService with a basic "Hello World!" endpoint
    - Set up ProfilesModule, ProfilesController, and ProfilesService for profile management
    - Add DTOs for creating and updating profiles with validation
    - Implement unit tests for controllers and services
    - Add end-to-end tests for the application
    - Configure TypeScript settings with tsconfig.json and tsconfig.build.json

---
+++ put.sh
@@ -0,0 +1,7 @@
+curl -X PUT -i http://localhost:3000/profiles/9dffbcd1-574a-4bbe-be81-86eec4151478 \
+	-H "Content-Type: application/json" \
+	-d '{
+	"name": "Ka",
+	
+	"description": "This JavaScript coder is a master of syntax, a weaver of the web, and a connoisseur of coffee. In the morning, I like to make a wheatgrass juice and set aside an hour to read up on the latest JS releases before starting my day. There'\''s just something electric about a fresh npm install, you know? Want to contribute to a new universe together?"
+}'
